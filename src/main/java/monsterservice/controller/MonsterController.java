9f79a953c417275014b5da4de392f72adcc21051
GolfZaaa <you@example.com>
2023-03-03 15:13:05 +0700

    Sprin-boots

---
+++ src/main/java/monsterservice/controller/MonsterController.java
@@ -0,0 +1,55 @@
+package monsterservice.controller;
+
+import monsterservice.model.Monster;
+import monsterservice.service.MonsterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("monster")
+public class MonsterController {
+
+    @Autowired
+    private MonsterService monsterService;
+
+    @GetMapping("/greeting")
+    public String getGreeting(){
+        return "Hi there ! DKub 9อวิรุทธ์ ไชยสงคราม";
+    }
+
+    @PostMapping("/create")
+    public Monster postCreate(@RequestBody Monster monster){
+        Monster response = monsterService.postCreateMonsterService(monster);
+        return response;
+    }
+
+    @GetMapping("/get-all")
+    public List<Monster> getAll(){
+        return monsterService.getAllMonsterService();
+    }
+
+    @GetMapping("/get-information")
+    public Monster getInformation(@RequestHeader Integer id){
+        Monster Information = monsterService.getInformation(id);
+        return Information;
+    }
+
+    @PutMapping("/update")
+    public Monster putUpdate(@RequestBody Monster monster){
+    return monsterService.getUpdateMonsterIdService(monster);
+    }
+
+    @DeleteMapping("/delete")
+    public  boolean delete(@RequestHeader Integer id){
+        return monsterService.deleteMonsterService(id);
+    }
+
+    @PutMapping("/attack")
+    public Monster putAttack(@RequestHeader Integer id,@RequestHeader Integer health ){
+        Monster attack = monsterService.attackMonsterService(id, health);
+        return attack;
+    }
+}
